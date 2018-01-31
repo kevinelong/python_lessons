fc01d2265a0fd6932825f1317ee552fc4557e42f
kevinelong2 <klong@drcloudemr.com>
2018-01-31 15:46:39 -0800

    update

--- section_01/interactive.py
+++ section_01/interactive.py
@@ -1,4 +1,15 @@
-i = ""
-while i != "quit":
-    i = raw_input("enter command")
-    print i
+
+while True:
+
+    user_text = raw_input("Enter Command: (dance, sleep or quit):")
+
+    if "quit" == user_text:
+        break #  Break out of the while loop
+    if "dance" == user_text:
+        print "\tDancing Complete."
+    elif "sleep" == user_text:
+        print "\tSlept."
+    elif "" == user_text:
+        print "Blank line is not a command."
+    else:
+        print "Unknown Command: " + user_text
